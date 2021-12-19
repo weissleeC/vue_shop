c4cb275221c47f744ffe3052e7886e6bc905aebc
weissleeC <876269104@qq.com>
2021-12-20 01:18:08 +0800

    feat:添加用户

--- src/views/user/Users.vue
+++ src/views/user/Users.vue
@@ -30,6 +30,7 @@
           >
         </el-col>
       </el-row>
+
       <!-- 用户列表 -->
       <el-table :data="userlist" style="width: 100%" stripe>
         <el-table-column type="index" label="#" width="50"></el-table-column>
@@ -72,6 +73,7 @@
           </template>
         </el-table-column>
       </el-table>
+
       <!-- 分页 -->
       <el-pagination
         @size-change="handleSizeChange"
@@ -83,18 +85,36 @@
         :total="total"
       >
       </el-pagination>
+
       <!-- 添加用户 -->
       <el-dialog
-        title="添加用户 "
+        title="添加用户"
         :visible.sync="dialogVisible"
-        :before-close="handleClose"
+        @close="addDialogClosed"
       >
-        <span>这是一段信息</span>
+        <el-form
+          :model="addForm"
+          :rules="addFormRules"
+          ref="addFormRef"
+          label-width="100px"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="addForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="addForm.password" show-password></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="addForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机" prop="mobile">
+            <el-input v-model="addForm.mobile"></el-input>
+          </el-form-item>
+        </el-form>
+
         <span slot="footer" class="dialog-footer">
           <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogVisible = false"
-            >确 定</el-button
-          >
+          <el-button type="primary" @click="addUser">确 定</el-button>
         </span>
       </el-dialog>
     </el-card>
@@ -104,6 +124,15 @@
 <script>
 export default {
   data() {
+    var checkMobile = (rule, value, callback) => {
+      if (value == '') {
+        callback(new Error('请输入手机号码'))
+      } else if (!this.isCellPhone(value)) {
+        callback(new Error('请输入正确的手机号码'))
+      } else {
+        callback()
+      }
+    }
     return {
       queryInfo: {
         query: '',
@@ -113,29 +142,77 @@ export default {
       userlist: [],
       total: 0,
       dialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: '',
+      },
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            min: 3,
+            max: 15,
+            message: '长度在 3 到 15 个字符',
+            trigger: 'blur',
+          },
+        ],
+        password: [
+          { required: true, message: '请输入用户密码', trigger: 'blur' },
+          {
+            min: 3,
+            max: 15,
+            message: '长度在 6 到 15 个字符',
+            trigger: 'blur',
+          },
+        ],
+        email: [
+          { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+          {
+            type: 'email',
+            message: '请输入正确的邮箱地址',
+            trigger: 'blur',
+          },
+        ],
+        mobile: [
+          {
+            required: true,
+            validator: checkMobile,
+            trigger: 'blur',
+          },
+        ],
+      },
     }
   },
   created() {
     this.getUsersList()
   },
   methods: {
-    // 弹窗关闭
-    handleClose(done) {
-      this.$confirm('确认关闭？')
-        .then((_) => {
-          done()
-        })
-        .catch((_) => {})
+    //检查手机号
+    isCellPhone(val) {
+      if (!/^1(3|4|5|6|7|8)\d{9}$/.test(val)) {
+        return false
+      } else {
+        return true
+      }
     },
 
-    // 获取用户
-    async getUsersList() {
-      const { data: res } = await this.$http.get('users', {
-        params: this.queryInfo,
+    // 对话框关闭重置表单
+    addDialogClosed() {
+      this.$refs.addFormRef.resetFields()
+    },
+
+    // 添加新用户
+    addUser() {
+      this.$refs.addFormRef.validate(async (valid) => {
+        if (!valid) return false
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) return this.$message.error(res.meta.msg)
+        this.$message.success(res.meta.msg)
+        this.getUsersList()
+        this.dialogVisible = false
       })
-      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
-      this.userlist = res.data.users
-      this.total = res.data.total
     },
 
     // 分页功能
@@ -146,6 +223,16 @@ export default {
     handleCurrentChange(val) {
       this.queryInfo.pagenum = val
       this.getUsersList()
+    },
+
+    // 获取用户
+    async getUsersList() {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo,
+      })
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.userlist = res.data.users
+      this.total = res.data.total
     },
 
     // 用户状态修改
